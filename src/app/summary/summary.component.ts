0385d4f4e0bc961c31aea0e2c194f2fd77076615
Jhay-Great <alemonai.jenef@gmail.com>
2024-07-10 13:48:25 +0000

    minor changes to ui

--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-summary',
@@ -10,5 +11,9 @@ import { Component } from '@angular/core';
 export class SummaryComponent {
 
   isConfirmed = false;
+
+  routing () {
+    
+  }
 
 }

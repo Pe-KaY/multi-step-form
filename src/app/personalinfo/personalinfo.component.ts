c5bbdf8281440798812bce529aa32bbb7a6850c5
IT Compliance <rhodaarthur48@gmail.com>
2024-07-10 12:48:37 -0700

    form validation completed

--- src/app/personalinfo/personalinfo.component.ts
+++ src/app/personalinfo/personalinfo.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-personalinfo',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,
+    CommonModule
+  ],
   templateUrl: './personalinfo.component.html',
   styleUrl: './personalinfo.component.css'
 })
-export class PersonalinfoComponent {
+export class PersonalinfoComponent implements OnInit{
+  personalInfoForm!: FormGroup;
+  pattern: any;
 
+  constructor(private formBuilder: FormBuilder){}
+
+  ngOnInit(): void {
+    this.personalInfoForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required, Validators.pattern('^[+]?[0-9]{1,3}[\\s]?[0-9]{3}[\\s]?[0-9]{3}[\\s]?[0-9]{4}')]],
+    })
+  }
+
+  nextStep(): void{
+    console.log('hello')
+    if (this.personalInfoForm.valid){
+      console.log(this.personalInfoForm.value)
+    }
+    else{
+      console.log('Form rejected')
+    }
+  }
 }
  

6e9c7cd3d826ed688c2ec6073abd61c83765ba98
kewablay <kojokewajunior123@gmail.com>
2024-07-10 14:30:28 +0000

    added addon list data

--- src/app/addons/addons.component.ts
+++ src/app/addons/addons.component.ts
@@ -5,8 +5,27 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './addons.component.html',
-  styleUrl: './addons.component.css'
+  styleUrl: './addons.component.css',
 })
 export class AddonsComponent {
-
+  addonsList = [
+    {
+      name: 'Online service',
+      description: 'Access to multiplayer games',
+      price: { monthly: 1, yearly: 10 },
+      checked: false,
+    },
+    {
+      name: 'Larger storage',
+      description: 'Extra 1TB of cloud save',
+      price: { monthly: 2, yearly: 20 },
+      checked: false,
+    },
+    {
+      name: 'Customizable profile',
+      description: 'Custom theme on your profile',
+      price: { monthly: 2, yearly: 20 },
+      checked: false,
+    },
+  ];
 }

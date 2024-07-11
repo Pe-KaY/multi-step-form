c953aabc50e8c2ec927feb87f3de176d806da9d9
IT Compliance <rhodaarthur48@gmail.com>
2024-07-10 17:08:17 -0700

    personal info functionality complemented

--- src/app/personalinfo/personalinfo.component.ts
+++ src/app/personalinfo/personalinfo.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { FormDataService } from '../service/form-data.service';
+import { FormData } from '../interfaces/form-data';
+import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-personalinfo',
   standalone: true,
-  imports: [ReactiveFormsModule,
-    CommonModule
-  ],
+  imports: [ReactiveFormsModule],
   templateUrl: './personalinfo.component.html',
   styleUrl: './personalinfo.component.css'
 })
@@ -17,20 +17,34 @@ export class PersonalinfoComponent implements OnInit{
   personalInfoForm!: FormGroup;
   pattern: any;
 
-  constructor(private formBuilder: FormBuilder){}
+  constructor(
+    private formBuilder: FormBuilder,
+    private formDataService: FormDataService,
+    private router: Router,
+  ){}
 
   ngOnInit(): void {
     this.personalInfoForm = this.formBuilder.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      phone: ['', [Validators.required, Validators.pattern('^[+]?[0-9]{1,3}[\\s]?[0-9]{3}[\\s]?[0-9]{3}[\\s]?[0-9]{4}')]],
+      phone: ['', [Validators.required, Validators.pattern(/^[0][2,5][4,5,0,9][0-9]{7}$/)]],
     })
+            
+
+
+    // load form data if available
+    const saveData: FormData = this.formDataService.getFormData();
+    if (saveData) {
+      this.personalInfoForm.patchValue(saveData);
+    }
   }
+  
+
 
   nextStep(): void{
-    console.log('hello')
     if (this.personalInfoForm.valid){
-      console.log(this.personalInfoForm.value)
+        this.formDataService.updateFormData(this.personalInfoForm.value);
+        this.router.navigate(['/plan']);
     }
     else{
       console.log('Form rejected')
